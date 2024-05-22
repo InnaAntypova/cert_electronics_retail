87d135cf100fd2f3aedd752b8cf77269e8bebf9e
Inna Antypova <nightallatiel@gmail.com>
2024-05-22 17:06:09 +0300

    Docker

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD')
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
